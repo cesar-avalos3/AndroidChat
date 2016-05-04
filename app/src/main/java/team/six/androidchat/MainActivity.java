68f99f0c78170091f8356512010790aa59d22553
aqknutsen <aqknutsen@gmail.com>
2016-05-04 16:27:27 -0500

    Minor Changes

--- app/src/main/java/team/six/androidchat/MainActivity.java
+++ app/src/main/java/team/six/androidchat/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by cesaravalos on 5/2/16.
@@ -12,6 +15,13 @@ public class MainActivity extends AppCompatActivity
 {
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+
+    private FragmentLogin login;
+    private FragmentSessions session;
+    private FragmentChat chat;
+
+
 
     protected void onCreate(Bundle savedInstance)
     {
@@ -24,15 +34,23 @@ public class MainActivity extends AppCompatActivity
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
 
+
     }
 
     private void setupViewPager(ViewPager viewPager)
     {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addFragment(new FragmentLogin(), "LOGIN");
-        viewPagerAdapter.addFragment(new FragmentSessions(), "SESSIONS");
-        viewPagerAdapter.addFragment(new FragmentChat(), "CHAT");
+
+        login = new FragmentLogin();
+        session = new FragmentSessions();
+        chat = new FragmentChat();
+
+        viewPagerAdapter.addFragment(login, "LOGIN");
+        viewPagerAdapter.addFragment(session, "SESSIONS");
+        viewPagerAdapter.addFragment(chat, "CHAT");
 
         viewPager.setAdapter(viewPagerAdapter);
     }
+
+
 }
